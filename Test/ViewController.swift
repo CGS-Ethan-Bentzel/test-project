8a34e84a811c7eeaed3490f8f09651dee84e6a03
Ethan Bentzel <42266@cgs.act.edu.au>
2015-07-29 11:35:01 +1000

    Initial commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Ethan Bentzel on 29/07/2015.
+//  Copyright (c) 2015 Strider Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
